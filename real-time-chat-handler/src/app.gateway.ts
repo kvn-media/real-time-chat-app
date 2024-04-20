ffc3024ce58479ca0a0a1b2667862add6e8053a9
kvn-media <kevin.subagio@gmail.com>
2024-04-20 14:34:13 +0700

    this is on going project

---
+++ real-time-chat-handler/src/app.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): void {
+    this.server.emit('msgToClient', text);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+}
